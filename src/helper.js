00316bf9086e49b3f638bd48221cdc9f629b1b8e
郑俊鑫 <18810544291@163.com>
2017-07-12 21:02:31 +0800

    delete useless helper

--- src/helper.js
+++
@@ -1,9 +0,0 @@
-export const getKeyByValue = (map, value) => {
-    let key = null
-    map.forEach((v, k) => {
-        if (v === value) {
-            key = k
-        }
-    })
-    return key
-}
