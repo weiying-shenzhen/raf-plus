89f95e055893d443c9b7845e0eee0c7faa83cb42
郑俊鑫 <18810544291@163.com>
2017-07-15 08:33:17 +0800

    clear useless dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,5 @@
 import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import hypothetical from 'rollup-plugin-hypothetical'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
@@ -11,14 +8,6 @@ export default {
     format: 'umd',
     moduleName: 'raf-plus',
     plugins: [
-        hypothetical({
-            allowRealFiles: true,
-            files: {
-                'node_modules/core-js/library/modules/es6.object.to-string.js': 'export default null'
-            }
-        }),
-        resolve({ jsnext: true, main: true }),
-        commonjs(),
         babel({
             babelrc: false,
             exclude: 'node_modules/**',
